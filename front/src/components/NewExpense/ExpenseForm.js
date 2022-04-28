c0c52794fa6bf1b4417c705b49d705f93e6c8a07
Mindaugas Krivickas <mkrivickas@gmail.com>
2022-04-29 01:19:10 +0300

    formos tvarkymas su inputais

--- front/src/components/NewExpense/ExpenseForm.js
+++ front/src/components/NewExpense/ExpenseForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import swal from 'sweetalert';
+import Swal from 'sweetalert2';
 
 import './ExpenseForm.css';
 
@@ -69,11 +69,23 @@ const ExpenseForm = (props) => {
             }),
         }).then(() => {
             // Once posted, the user will be notified
-            swal({
-                title: 'Šaunu!',
-                text: 'Jūsų išlaidos įvestos!',
-                icon: 'success',
-                button: 'Gerai',
+            Swal.fire({
+                title: 'Ar esate tikri?',
+                text: 'Dėmesio! Duomenys bus įrašyti.',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Įrašyti',
+                cancelButtonText: 'Atšaukti',
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    Swal.fire(
+                        'Įrašyta!',
+                        'Įrašas įtrauktas į žurnalą.',
+                        'success'
+                    );
+                }
             });
             // alert('Your incomes was added successfully');
         });
@@ -85,8 +97,8 @@ const ExpenseForm = (props) => {
     };
 
     return (
-        <form className="container-fluid" onSubmit={submitHandler}>
-            <div className='new-expense__controls row d-flex justify-content-between'>
+        <form className='container-fluid' onSubmit={submitHandler}>
+            <div className='new-expense__controls row d-flex justify-content-center'>
                 <div className='new-expense__control col-5'>
                     <label className='new-expense__category'>Kategorija</label>
                     <select onChange={categoryChangeHandler}>
@@ -95,7 +107,9 @@ const ExpenseForm = (props) => {
                         <option value='hygiene'>Higiena</option>
                         <option value='transport'>Transportas</option>
                         <option value='automotive'>Automobilis</option>
-                        <option value='municipal'>Komunalkos</option>
+                        <option value='municipal'>
+                            Komunaliniai mokesčiai
+                        </option>
                         <option value='services'>Paslaugos</option>
                         <option value='education'>Mokymai</option>
                         <option value='hobby'>Pomėgiai</option>
@@ -113,8 +127,9 @@ const ExpenseForm = (props) => {
                         required
                         min='0.01'
                         step='0.01'
-                        maxlength='10'
+                        maxlength='7'
                         minlength='1'
+                        pattern='[0-9]{6,9}'
                         placeholder='Išlaidų suma, €'
                         value={enteredAmount}
                         onChange={amountChangeHandler}
@@ -128,21 +143,21 @@ const ExpenseForm = (props) => {
                         name='expenseDate'
                         min='2019-01-01'
                         max='2099-12-31'
-                        placeholder='MMMM-MM-DD'
+                        placeholder='MMMM-mm-dd'
                         value={enteredDate}
                         onChange={dateChangeHandler}
                     />
                 </div>
                 <div className='new-expense__control col-5'>
-                    <label>Išlaidos</label>
+                    <label>Pavadinimas</label>
                     <input
                         type='text'
                         name='expenseName'
                         required
-                        maxlength='20'
-                        minlength='5'
+                        maxlength='40'
+                        minlength='3'
                         placeholder='Išlaidų pavadinimas'
-                        pattern='^[a-zA-Z0-9_.-]*$'
+                        pattern='^[\p{L},.0-9\s-]+$'
                         value={enteredTitle}
                         onChange={titleChangeHandler}
                     />
