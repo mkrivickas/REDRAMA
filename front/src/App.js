3271ea64c34a93c3a9a050f111b0e6d0c14500d8
dannge <danngess@gmail.com>
2022-05-02 12:21:53 +0300

    meniuNepadaryta

--- front/src/App.js
+++ front/src/App.js
@@ -8,9 +8,9 @@ import { useState } from 'react';
 import Swal from 'sweetalert2';
 
 function App() {
-	let [currentUser, setCurrentUser] = useState("Ramunas")
+	let [ currentUser, setCurrentUser ] = useState('Ramunas');
 
-	function logout(){
+	function logout() {
 		Swal.fire({
 			title: 'Ar tikrai norite atsijungti?',
 			text: 'Duomenys bus įrašyti.',
@@ -19,31 +19,41 @@ function App() {
 			confirmButtonColor: '#3085d6',
 			cancelButtonColor: '#d33',
 			confirmButtonText: 'Atsijungti',
-			cancelButtonText: 'Atšaukti',
+			cancelButtonText: 'Atšaukti'
 		}).then((result) => {
 			if (result.isConfirmed) {
-				setCurrentUser("")
+				setCurrentUser('');
 			}
 		});
 	}
 	return (
-
 		<div className="App">
-			{!currentUser &&
-			<div>
-				<h1>Registruotis</h1>
-				<Register setCurrentUser={setCurrentUser}/>
-				<h1>Prisijungti</h1>
-				<Login  setCurrentUser={setCurrentUser}/>
-			</div>}
-			{currentUser &&
-			<div className='appMainPage'>
-				<div className='appLogoutHeader'><button onClick={()=>{logout()}} className="appLogoutButton" >Atsijungti</button></div>
-				<h1>Išlaidos</h1>
-				<NewExpense />
-				<h1>Pajamos</h1>
-				<IncomeForm />
-			</div>}
+			{!currentUser && (
+				<div>
+					<h1>Registruotis</h1>
+					<Register setCurrentUser={setCurrentUser} />
+					<h1>Prisijungti</h1>
+					<Login setCurrentUser={setCurrentUser} />
+				</div>
+			)}
+			{currentUser && (
+				<div className="appMainPage">
+					<div className="appLogoutHeader">
+						<button
+							onClick={() => {
+								logout();
+							}}
+							className="appLogoutButton"
+						>
+							Atsijungti
+						</button>
+					</div>
+					<h1>Išlaidos</h1>
+					<NewExpense />
+					<h1>Pajamos</h1>
+					<IncomeForm />
+				</div>
+			)}
 		</div>
 	);
 }
