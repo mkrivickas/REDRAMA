b053d73bb4c9097bbc26516e005a97a4bd4fe8e4
Mindaugas Krivickas <mkrivickas@gmail.com>
2022-05-11 14:30:17 +0300

    expense form

--- front/src/components/NewExpense/ExpenseList.js
+++ front/src/components/NewExpense/ExpenseList.js
@@ -5,53 +5,42 @@ import './ExpenseList.css';
 
 const ExpenseList = ({ expense, deleteExpense, editExpense }) => (
     <div className='ExpenseList-container fluid'>
-        <div className='ExpenseList-row' key={expense._id}>
-            <div className='ExpenseDate-List'>{expense.Date.slice(0, 10)}</div>
-            <div className='ExpenseCategory-List'>{expense.Category}</div>
-            <div className='ExpenseName-List'>{expense.Name}</div>
-            <div className='ExpenseAmount-List'>{expense.Amount}€</div>
-            <div>
-                <button
-                    className='ExpenseListEdit-button'
-                    onClick={() => {
-                        window.scrollTo(0, 0);
-                        editExpense(expense);
-                        /* 									props.openModal(); */
-                    }}
-                >
-                    <FaRegEdit />
-                </button>
-            </div>
-            <div>
-                <button
-                    onClick={() => deleteExpense(expense._id)}
-                    className='ExpenseListDelete-button'
-                >
-                    <FaTrash />
-                </button>
-            </div>
-            // // ////////////////////////////////
-            {/* <table className='ExpenseList-table'>
-                <tbody>
-                    {props.expense.length > 0 ? (
-                        props.expense.map((expense) => (
-                            <tr key={expense._id}>
-                                <td></td>
-                                <td></td>
-
-                                <td></td>
-                                <td></td>
-                            </tr>
-                        ))
-                    ) : (
-                        <tr>
-                            <td colSpan={3}>Nėra pajamų</td>
-                        </tr>
-                    )}
-                </tbody>
-            </table> */}
-            ////////////////////////////////
-        </div>
+        {expense.length > 0 ? (
+            expense.map((expense) => (
+                <div className='ExpenseList-row d-flex' key={expense._id}>
+                    <div className='ExpenseDate-List'>
+                        {expense.Date.slice(0, 10)}
+                    </div>
+                    <div className='ExpenseCategory-List'>
+                        {expense.Category}
+                    </div>
+                    <div className='ExpenseName-List'>{expense.Name}</div>
+                    <div className='ExpenseAmount-List'>{expense.Amount}€</div>
+                    <div>
+                        <button
+                            className='ExpenseListEdit-button'
+                            onClick={() => {
+                                window.scrollTo(0, 0);
+                                editExpense(expense);
+                                /* 									props.openModal(); */
+                            }}
+                        >
+                            <FaRegEdit />
+                        </button>
+                    </div>
+                    <div>
+                        <button
+                            onClick={() => deleteExpense(expense._id)}
+                            className='ExpenseListDelete-button'
+                        >
+                            <FaTrash />
+                        </button>
+                    </div>
+                </div>
+            ))
+        ) : (
+            <div>Nera islaidu</div>
+        )}
     </div>
 );
 
