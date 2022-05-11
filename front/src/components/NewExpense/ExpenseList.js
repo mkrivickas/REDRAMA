3094d78e6756f8b18ae104b872044e9efb1362d0
Mindaugas Krivickas <mkrivickas@gmail.com>
2022-05-11 14:22:01 +0300

    black expense page

--- front/src/components/NewExpense/ExpenseList.js
+++ front/src/components/NewExpense/ExpenseList.js
@@ -3,53 +3,44 @@ import { FaTrash } from 'react-icons/fa';
 import { FaRegEdit } from 'react-icons/fa';
 import './ExpenseList.css';
 
-const ExpenseList = (props) => (
+const ExpenseList = ({ expense, deleteExpense, editExpense }) => (
     <div className='ExpenseList-container fluid'>
-        <div className='ExpenseList-row'>
-            <table className='ExpenseList-table'>
+        <div className='ExpenseList-row' key={expense._id}>
+            <div className='ExpenseDate-List'>{expense.Date.slice(0, 10)}</div>
+            <div className='ExpenseCategory-List'>{expense.Category}</div>
+            <div className='ExpenseName-List'>{expense.Name}</div>
+            <div className='ExpenseAmount-List'>{expense.Amount}€</div>
+            <div>
+                <button
+                    className='ExpenseListEdit-button'
+                    onClick={() => {
+                        window.scrollTo(0, 0);
+                        editExpense(expense);
+                        /* 									props.openModal(); */
+                    }}
+                >
+                    <FaRegEdit />
+                </button>
+            </div>
+            <div>
+                <button
+                    onClick={() => deleteExpense(expense._id)}
+                    className='ExpenseListDelete-button'
+                >
+                    <FaTrash />
+                </button>
+            </div>
+            // // ////////////////////////////////
+            {/* <table className='ExpenseList-table'>
                 <tbody>
                     {props.expense.length > 0 ? (
                         props.expense.map((expense) => (
                             <tr key={expense._id}>
-                                
-                                <td>
-                                    <div className='ExpenseCategory-List'>
-                                        {expense.Category}
-                                    </div>
-                                </td>
-                                <td>
-                                    <div className='ExpenseName-List'>
-                                        {expense.Name}
-                                    </div>
-                                    <div className='ExpenseDate-List'>
-                                        {expense.Date.slice(0, 10)}
-                                    </div>
-                                </td>
-                                <td className='ExpenseAmount-List'>
-                                    {expense.Amount}€
-                                </td>
-                                <td>
-                                    <button
-                                        className='ExpenseListEdit-button'
-                                        onClick={() => {
-                                            window.scrollTo(0, 0);
-                                            props.editExpense(expense);
-                                            /* 									props.openModal(); */
-                                        }}
-                                    >
-                                        <FaRegEdit />
-                                    </button>
-                                </td>
-                                <td>
-                                    <button
-                                        onClick={() =>
-                                            props.deleteExpense(expense._id)
-                                        }
-                                        className='ExpenseListDelete-button'
-                                    >
-                                        <FaTrash />
-                                    </button>
-                                </td>
+                                <td></td>
+                                <td></td>
+
+                                <td></td>
+                                <td></td>
                             </tr>
                         ))
                     ) : (
@@ -58,7 +49,8 @@ const ExpenseList = (props) => (
                         </tr>
                     )}
                 </tbody>
-            </table>
+            </table> */}
+            ////////////////////////////////
         </div>
     </div>
 );
